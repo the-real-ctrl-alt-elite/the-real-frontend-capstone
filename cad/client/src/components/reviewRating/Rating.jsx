5b68b82caa33992d4d3dfb27f73915bc4e2e5a9c
Robert Potter <robert.potter.741@gmail.com>
2024-10-10 23:07:25 -0400

    Attempt to make the linter happy and fail

--- cad/client/src/components/reviewRating/Rating.jsx
+++ cad/client/src/components/reviewRating/Rating.jsx
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from "react";
-import ReviewList from "./components/ReviewList.jsx";
-import ReviewTile from "./components/ReviewTile.jsx";
-import ProductBreakdown from "./components/ProductBreakdown.jsx";
-import RatingBreakdown from "./components/RatingBreakdown.jsx";
-import SortOptions from "./components/SortOptions.jsx";
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-require('dotenv').config();
+import ReviewList from './components/ReviewList';
+import ProductBreakdown from './components/ProductBreakdown';
+import RatingBreakdown from './components/RatingBreakdown';
+import SortOptions from './components/SortOptions';
 
 const mockData = {
-  product: "2",
+  product: '2',
   page: 0,
   count: 5,
   results: [
@@ -18,18 +16,18 @@ const mockData = {
       summary: "I'm enjoying wearing these shades",
       recommend: false,
       response: null,
-      body: "Comfortable and practical.",
-      date: "2019-04-14T00:00:00.000Z",
-      reviewer_name: "shortandsweeet",
+      body: 'Comfortable and practical.',
+      date: '2019-04-14T00:00:00.000Z',
+      reviewer_name: 'shortandsweeet',
       helpfulness: 5,
       photos: [
         {
           id: 1,
-          url: "urlplaceholder/review_5_photo_number_1.jpg",
+          url: 'urlplaceholder/review_5_photo_number_1.jpg',
         },
         {
           id: 2,
-          url: "urlplaceholder/review_5_photo_number_2.jpg",
+          url: 'urlplaceholder/review_5_photo_number_2.jpg',
         },
         // ...
       ],
@@ -37,34 +35,34 @@ const mockData = {
     {
       review_id: 3,
       rating: 4.5,
-      summary: "I am liking these glasses",
+      summary: 'I am liking these glasses',
       recommend: true,
       response: "Glad you're enjoying the product!",
-      //adding length to test limited display functionality for ReviewTile
+      // adding length to test limited display functionality for ReviewTile
       body: "They are very dark. But that's good because I'm in very sunny spots. They are very dark. But that's good because I'm in very sunny spots. They are very dark. But that's good because I'm in very sunny spots. They are very dark. But that's good because I'm in very sunny spots. They are very dark. But that's good because I'm in very sunny spots.",
-      date: "2019-06-23T00:00:00.000Z",
-      reviewer_name: "bigbrotherbenjamin",
+      date: '2019-06-23T00:00:00.000Z',
+      reviewer_name: 'bigbrotherbenjamin',
       helpfulness: 12,
       photos: [],
     },
     {
       review_id: 6,
       rating: 0.5,
-      summary: "These are booty!",
+      summary: 'These are booty!',
       recommend: false,
       response: null,
       body: "I've never had a worse experience in my life! Delivery was 5 minutes late! Unebleieveabletjet!",
-      date: "2019-08-19T00:00:00.000Z",
-      reviewer_name: "angryshopper69",
+      date: '2019-08-19T00:00:00.000Z',
+      reviewer_name: 'angryshopper69',
       helpfulness: 0,
       photos: [
         {
           id: 1,
-          url: "urlplaceholder/review_5_photo_number_1.jpg",
+          url: 'urlplaceholder/review_5_photo_number_1.jpg',
         },
         {
           id: 2,
-          url: "urlplaceholder/review_5_photo_number_2.jpg",
+          url: 'urlplaceholder/review_5_photo_number_2.jpg',
         },
         // ...
       ],
@@ -73,21 +71,21 @@ const mockData = {
       review_id: 9,
       rating: 5.0,
       summary:
-        "THE BEST EVER BY FAR I CANT BELIEVE IT TOOK ME THIS LONG TO ORDER A PAIR OF THESE INCREDIBLE THINGS",
+        'THE BEST EVER BY FAR I CANT BELIEVE IT TOOK ME THIS LONG TO ORDER A PAIR OF THESE INCREDIBLE THINGS',
       recommend: true,
       response: "Sir this is a Wendy's.",
       body: "Yo, I love these so much! They tickle my fancy and my nose! And since I love sneezing more than pretty much anything else in the world, these are right up my alley! Have you ever sat and wondered about why sunglasses take away sunsneezes? Not with these! Give them ago and you'll be able to see fine while also sneezing like a dog at play! which is to say, a lot! These sneezers are incredible! Hell yeah!",
-      date: "2019-12-26T00:00:00.000Z",
-      reviewer_name: "dogmanthemandog",
+      date: '2019-12-26T00:00:00.000Z',
+      reviewer_name: 'dogmanthemandog',
       helpfulness: 5,
       photos: [
         {
           id: 1,
-          url: "urlplaceholder/review_5_photo_number_1.jpg",
+          url: 'urlplaceholder/review_5_photo_number_1.jpg',
         },
         {
           id: 2,
-          url: "urlplaceholder/review_5_photo_number_2.jpg",
+          url: 'urlplaceholder/review_5_photo_number_2.jpg',
         },
         // ...
       ],
@@ -96,29 +94,18 @@ const mockData = {
   ],
 };
 
-const Rating = (props) => {
-
+const Rating = () => {
   useEffect(() => {
-
     const params = {
       page: 1,
       count: 10,
       sort: 'relevant',
-      product_id: 1
-    }
-    axios.get(`${process.env.API_URL}/reviews/1/10/'relevant'/1`, {
-      params: params,
-      headers: {
-        "Authorization": `${process.env.API_KEY}`
-      }
-    })
-    .then((response) => {
-      console.log(response)
-    })
-  })
+      product_id: 1,
+    };
+  }, []);
 
   const [reviews, setReviews] = useState(mockData.results);
-  //BRD display 2 tiles at a time
+  // BRD display 2 tiles at a time
   const [activeReviews, setActiveReviews] = useState(reviews.slice(0, 2));
 
   const fn = {
@@ -128,15 +115,15 @@ const Rating = (props) => {
   };
 
   return (
-    <div style={{ padding: "1rem", width: '66%'}}>
+    <div style={{ padding: '1rem', width: '66%', margin: 'auto' }}>
       <h3>RATINGS & REVIEWS</h3>
       <div>
-        <div className="rating-container">
-          <div className="reviewsLeft">
+        <div className='rating-container'>
+          <div className='reviewsLeft'>
             <RatingBreakdown fn={fn} />
             <ProductBreakdown />
           </div>
-          <div className="reviewsRight">
+          <div className='reviewsRight'>
             <SortOptions fn={fn} />
             <ReviewList fn={fn} />
           </div>
