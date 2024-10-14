6146faa86085e72cf84211538eb0cff69d2b5bc6
Robert Potter <robert.potter.741@gmail.com>
2024-10-13 22:08:31 -0400

    Implement modular ModalBackground and ModalFrame for consistency

---
+++ cad/client/src/components/reviewRating/components/ModalBackground.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ModalFrame from './ModalFrame';
+
+const ModalBackground = ({
+  background,
+  closeModal,
+  component,
+  componentProps,
+  padding,
+  top,
+}) => {
+  // top is only used for pages with greater than 100vh functionality
+  // otherwise it can be left at 0;
+  // background can also be set with any color scheme
+  // closeModal needs to be an event that swaps the model state from
+  // active to inactive, for example setModalState(!modalState)
+  // which gets passed all the way down to an x button in modelFrame
+
+  const usedTop = top || '0';
+  const usedBackground = background || 'rgba(22,22,22,0.5)';
+  const usedPadding = padding || '2rem';
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: `${usedTop}`,
+        left: '0',
+        width: '100vw',
+        height: '100vh',
+        background: `${usedBackground}`,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: `${usedPadding}`,
+
+      }}
+    >
+      <ModalFrame
+        border='4px dotted green'
+        component={component}
+        componentProps={componentProps}
+        closeModal={closeModal}
+        padding='10rem'
+      />
+    </div>
+  );
+};
+
+export default ModalBackground;
---
+++ cad/client/src/components/reviewRating/components/ModalFrame.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const ModalFrame = ({
+  border,
+  buttonText,
+  component,
+  componentProps,
+  closeModal,
+  fontColor,
+  padding,
+}) => {
+  // provide inline css or use these defaults
+  const usedBorder = border || '1px solid white';
+  const usedButtonText = buttonText || 'X';
+  const usedPadding = padding || 'inherit';
+  const usedFontColor = fontColor || 'white';
+  return (
+    <div
+      style={{
+        position: 'relative',
+        maxHeight: '75vh',
+        maxWidth: '75vw',
+        padding: `${usedPadding}`,
+        border: `${usedBorder}`,
+      }}
+    >
+      {component(componentProps)}
+      <button
+        type='button'
+        onClick={() => closeModal()}
+        style={{
+          position: 'absolute',
+          top: '0',
+          right: '0',
+          borderRadius: '1rem',
+          height: '2rem',
+          width: '2rem',
+          background: 'rgba(22,22,22,0.5)',
+          boxShadow: '0 0 2px 2px rgba(22,22,22,0.5)',
+          color: `${usedFontColor}`,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {usedButtonText}
+      </button>
+    </div>
+  );
+};
+
+export default ModalFrame;
