7763538c632e02188ed76c2e2b1e66ac982378a0
Robert Potter <robert.potter.741@gmail.com>
2024-10-14 12:02:50 -0400

    Increase modularity by giving more options

--- cad/client/src/components/reviewRating/components/ModalBackground.jsx
+++ cad/client/src/components/reviewRating/components/ModalBackground.jsx
@@ -3,10 +3,14 @@ import ModalFrame from './ModalFrame';
 
 const ModalBackground = ({
   background,
+  border,
+  buttonText,
   closeModal,
   component,
   componentProps,
+  fontColor,
   padding,
+  innerPadding,
   top,
 }) => {
   // top is only used for pages with greater than 100vh functionality
@@ -19,6 +23,10 @@ const ModalBackground = ({
   const usedTop = top || '0';
   const usedBackground = background || 'rgba(22,22,22,0.5)';
   const usedPadding = padding || '2rem';
+  const usedBorder = border || '1px solid white';
+  const usedButtonText = buttonText || 'X';
+  const usedFontColor = fontColor || 'white';
+  const usedInnerPadding = innerPadding || '2rem';
 
   return (
     <div
@@ -38,11 +46,13 @@ const ModalBackground = ({
       }}
     >
       <ModalFrame
+        border={usedBorder}
+        buttonText={usedButtonText}
         component={component}
         componentProps={componentProps}
         closeModal={closeModal}
-        padding='0rem'
-        border='none'
+        fontColor={usedFontColor}
+        padding={usedInnerPadding}
       />
     </div>
   );
