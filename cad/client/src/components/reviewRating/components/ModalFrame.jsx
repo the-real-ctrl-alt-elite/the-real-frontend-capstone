edc08d316373a211de48c4d79ac00dab5b7be47f
Robert Potter <robert.potter.741@gmail.com>
2024-10-14 13:41:16 -0400

    Change modal frame default to none

--- cad/client/src/components/reviewRating/components/ModalFrame.jsx
+++ cad/client/src/components/reviewRating/components/ModalFrame.jsx
@@ -10,7 +10,7 @@ const ModalFrame = ({
   padding,
 }) => {
   // provide inline css or use these defaults
-  const usedBorder = border || '1px solid white';
+  const usedBorder = border || 'none';
   const usedButtonText = buttonText || 'X';
   const usedPadding = padding || 'inherit';
   const usedFontColor = fontColor || 'white';
