07b170e0f57b1a5cd9655461b93ad50c4ea83945
Skyla Reiner <sysreiner@gmail.com>
2024-10-21 15:42:44 -0700

    forgot to add label to product card

--- cad/client/src/components/relatedComparison/RelatedCard.jsx
+++ cad/client/src/components/relatedComparison/RelatedCard.jsx
@@ -30,7 +30,7 @@ const RelatedCard = ({
         <button onClick={() => handleCompareClick()} aria-label='compare-item-btn' type='button' label='compare-item' className='action-item-btn'>
           <i className='fa-solid fa-star-sharp fa-xs' style={{ color: '#ffffff', padding: '3px' }} />
         </button>
-        <div className='product-card-content' role='button' tabIndex={0} onKeyDown={handleItemClick} onClick={handleItemClick}>
+        <div className='product-card-content' role='button' aria-label='product-card-btn' tabIndex={0} onKeyDown={handleItemClick} onClick={handleItemClick}>
           <img className='product-card-img' src={photos?.url} alt='product-item-img' />
           <div className='product-card-details'>
             <h6>{category.toUpperCase()}</h6>
